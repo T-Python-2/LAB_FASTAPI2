8a4ed9a1a23f0179196a2e36e2024a70a5b11b82
badr <badr.fadul.jed.k1@tuwaiq.edu.sa>
2022-05-01 17:42:14 +0300

    fastlab2_lab

--- main.py
+++ main.py
@@ -1,82 +1,50 @@
+from typing import List
 from fastapi import FastAPI
 from pydantic import BaseModel
-from typing import List
+
 
 app = FastAPI()
 
-
-cityTemp = {"Riyadh" : 40 ,"Abha" : 23, "Jedda" : 45, "Dammam" : 37}
-
-#http Method GET
-@app.get("/")
-def index():
-    return {"msg": f"cities tempreture {cityTemp}"}
-
-#http Method GET with path parameters
-@app.get("/tempreture/{city}")
-#query parameters , any parameter that is not a path parameter
-def index(city : str ):
-    return {"msg": f"{city} tempreture is: {cityTemp[city]}"}
+class city(BaseModel):
+    name : str
+    weather: int
 
 
-"""
-#user model
-class User(BaseModel):
-    user_name : str
-    age : int = 0
-    email: str
-
-#response model 
 class ResponseModel(BaseModel):
     msg : str
-    content : List[User]
+    content : List[city]
 
 
-#CRUD operations on Model - Create:Post Read:Get Update:Put Delete:Delete
+cities : list = []
 
 
-users : list = []
 
-#http Method GET
 @app.get("/")
 def index():
-    return {"msg":"This is the index , or the home page"}
-
-
-#http Method GET with path parameters
-@app.get("/hello/{name}")
-#query parameters , any parameter that is not a path parameter
-def index(name : str, toUpper:bool = False):
-    return {"msg": f"Hello {name}"}
-
-
-#http Method Get / read
-@app.get("/users", response_model=ResponseModel)
-def get_users():
-    return ResponseModel(msg="All users", content=users)
+    return {"msg":"This is Home page"}
 
 
 
-
-#http Method Post / create
-@app.post("/user/create", response_model=ResponseModel)
-def create_user(user : User):
-    users.append(user)
-    return ResponseModel(msg="Added new user", content=users)
+@app.post("/city/create",response_model=ResponseModel)
+def add_city(city : city):
+    cities.append(city)
+    return ResponseModel(msg="New City Has Been Added" , content=cities)
 
 
-
-#http Method Put / update
-@app.put("/users/update/{index}", response_model=ResponseModel)
-def update_user(user:User, index:int):
-    users[index] = user
-    return ResponseModel(msg="Update user successfully", content=users)
+@app.get("/cities",response_model=ResponseModel)
+def get_all_cities():
+    return ResponseModel(msg="All Cities",content=cities)
 
 
+@app.put("/cities/update/{index}",response_model=ResponseModel)
+def update_city(city:city ,index:int):
+    cities[index] = city
+    return ResponseModel(msg="City Updated Successfully",content=cities)
 
 
-@app.delete("/users/delete/{index}", response_model=ResponseModel)
-def delete_user(index:int):
-    del users[index]
-    return ResponseModel(msg=f"deleted user with id: {index}", content=users)
-"""
+
+@app.delete("/cities/delete/{index}",response_model=ResponseModel)
+def delete_city(index:int):
+   del cities[index]
+   return ResponseModel(msg=f" City with Index {index} Has Been Deleted",content=cities)
+
